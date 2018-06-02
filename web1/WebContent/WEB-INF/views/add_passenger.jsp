d482e29acdd4fc0499d2fcba15b443211a23a6bd
Alan Villa <a19i23villa@gmail.com>
2018-06-02 16:54:57 -0500

    added form info to passenger object

--- web1/WebContent/WEB-INF/views/add_passenger.jsp
+++ web1/WebContent/WEB-INF/views/add_passenger.jsp
@@ -12,10 +12,11 @@
 
 	<div class="container">
 		<div class="title">Add a passenger</div>
-		<fieldset>
+		<form action="AddPassenger" method="post">
 
-			<legend>Passenger details</legend>
-			<form action="AddPassenger" method="post">
+			<fieldset>
+				<legend>Passenger details</legend>
+
 				<div class="inputfield">
 					<label for="first-name">First name:</label> 
 					<input name="first-name" type="text"></input>
@@ -26,21 +27,22 @@
 				</div>
 				<div class="inputfield">
 					<label for="dob">Date of birth:</label> 
-					<input name="dob" type="text"></input>
+					<input name="dob" type="date"></input>
 				</div>
 				<div class="inputfield">
-					<label for="gender" class="inputLabel">Gender:</label>
+					<label for="gender" class="inputLabel">Gender:</label> 
 					<select name="gender">
 						<option value="Male">Male</option>
 						<option value="Female">Female</option>
 					</select>
 				</div>
-			</form>
-		</fieldset>
-		
-		<div class="inputField" id="submitField">
-			<input	id="submitBtn" type="submit" value="Add new passenger"></input>
-		</div>
+			</fieldset>
+
+			<div class="inputField" id="submitField">
+				<input id="submitBtn" type="submit" value="Add new passenger"></input>
+			</div>
+		</form>
+
 
 	</div>
 </body>
--- web1/src/com/airline/controllers/AddPassenger.java
+++ web1/src/com/airline/controllers/AddPassenger.java
@@ -1,6 +1,10 @@
 package com.airline.controllers;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +12,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.airline.models.Gender;
+import com.airline.models.Passenger;
 
 /**
  * Servlet implementation class AddPassenger
@@ -37,7 +44,49 @@ public class AddPassenger extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Passenger p;
+		String firstName = request.getParameter("first-name");
+		String lastName = request.getParameter("last-name");		
+		String dobString = request.getParameter("dob");
+		String gender = request.getParameter("gender");
+		
+		validateInput(request, firstName, "firstNameError");
+		validateInput(request, lastName, "lastNameError");
+		validateInput(request, dobString, "dobError");
+		
+		System.out.println("First name: " + firstName);
+		
+		if (request.getAttribute("error")!=null && (boolean)request.getAttribute("error")) {
+			
+		} else {
+			p = new Passenger();
+			p.setFirstName(firstName);
+			p.setLastName(lastName);
+			
+			Date dob;
+			try {
+				dob = getDateFromString(dobString);
+				p.setDob(dob);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			p.setGender(Gender.valueOf(gender));
+		}
+		
+	}
+
+	private Date getDateFromString(String str) throws ParseException {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");	
+		Date date = df.parse(str);
+		return date;	
+	}
+
+	private void validateInput(HttpServletRequest request, String attributeName, String errorName) {
+		if (attributeName.length() == 0 ) {
+			System.out.println(errorName.toString());
+			request.setAttribute("error", true);
+			request.setAttribute(errorName, true);
+		}
 	}
 
 }
